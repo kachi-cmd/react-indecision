9f007f3fcce90b050ff0b815ebbe0b6f49863412
emmanuel-kachi <emmagrt1@gmail.com>
2020-11-26 20:26:18 +0100

    up

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const path = require('path');
-//const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+process.env.NODE_ENV = process.env.NODE_ENV ||'development'
+
 
 module.exports = (env) => {
   const isProduction = env === 'production';
-  //const CSSExtract = new ExtractTextPlugin('styles.css');
 
   return {
     entry: ['@babel/polyfill','./src/app.js'],
@@ -24,7 +25,6 @@ module.exports = (env) => {
       }
       }, {
         test: /\.s?css$/,
-        //use: CSSExtract.extract({
           use: [
             {
               loader: MiniCssExtractPlugin.loader,
@@ -43,11 +43,9 @@ module.exports = (env) => {
               }
             }
           ]
-        //})
       }]
     },
     plugins: [
-      //CSSExtract
       new MiniCssExtractPlugin({
         filename: '[name].css',
         chunkFilename: '[id].css',
@@ -67,31 +65,3 @@ module.exports = (env) => {
 
 
 
-// const path = require('path')
-
-// module.exports={
-//     entry :['@babel/polyfill','./src/app.js'],
-//     output:{
-//         path: path.join(__dirname,'public'),
-//         filename :'bundle.js'   
-//     },
-//   module : {
-//       rules : [{
-//         loader : 'babel-loader',
-//         test : /\.js$/,
-//         exclude : /node_modules/
-//       }, {
-//         test : /\.s?css$/,
-//         use : [
-//           'style-loader',
-//           'css-loader',
-//           'sass-loader'
-//         ]
-//       }]
-//   }, 
-//   devtool : 'cheap-module-eval-source-map' ,
-//   devServer : {
-//     contentBase :  path.join(__dirname,'public'),
-//     historyApiFallback : true
-//   }
-// }
